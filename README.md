a49c82d1f322b1c0fdf3fb4c198af09a4a052b9b
TJ Bindseil <bindseil@audible.com>
2023-04-15 12:20:35 -0600

    initial commit of typescript socket server

---
+++ README.md
@@ -0,0 +1,9 @@
+overview:
+
+* Serves basic webpage on localhost:8080/.
+* This webpage connects to a socket upon loading and disconnects upon unloading.
+* Server logs connections and disconnections
+
+run with:
+dev => `npm run build && npm run start`
+normal => `npm run dev`
---
+++ server.ts
@@ -0,0 +1,24 @@
+import express, { Express, Request, Response } from 'express';
+import http from 'http';
+import socket_factory, {Server, Socket} from 'socket.io';
+
+const app: Express = express();
+const server = http.createServer(app);
+const port = process.env.PORT || 8080;
+
+const io: Server = new socket_factory.Server(server);
+
+app.get('/', (_req: Request, res: Response) => {
+  res.send('Express + TypeScript Server');
+});
+
+io.on('connection', (socket: Socket) => {
+  console.log('user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+})
+
+server.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
